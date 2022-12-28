a36eeb8626e45fcf8a60607ef04ea4c3e3c33eb7
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-12-28 22:56:24 +0900

    init: initial project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './styles/main.scss';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
