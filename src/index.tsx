0b0e4ef2f25d0fb0140fcadf19323d49b6d7a831
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-03 17:58:40 +0900

    reafactor: 타입스크립트로 변환

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './styles/main.scss';
-import './index.css';
 import App from './App';
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
