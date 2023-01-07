16e7afffbd6418d28d26cf8345f384569d833817
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-07 23:26:24 +0900

    feat: React-Reducer 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/main.scss';
 import App from './App';
 import React from 'react';
+import {Provider} from 'react-redux';
+import rootReducer from './store';
+import {legacy_createStore} from 'redux';
 
+const store = legacy_createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
