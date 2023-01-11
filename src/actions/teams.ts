e4a40c1b8960ee1f7e6fe5707148e9a4f82e3f7e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-11 20:37:22 +0900

    refactor: 국가 선택 시 국가코드 의존성 제거 및 타입 변경(string -> Team)

--- src/actions/teams.ts
+++ src/actions/teams.ts
@@ -1,16 +1,16 @@
-import {Teams} from '../types';
+import {Team} from '../types';
 
-const ADD_TEAM = 'addteam' as const;
-const DELETE_TEAM = 'deleteteam' as const;
+export const ADD_TEAM = 'team/addteam' as const;
+export const DELETE_TEAM = 'team/deleteteam' as const;
 
-export const addTeam = (data: Teams) => {
+export const addTeam = (data: Team) => {
     return {
         type: ADD_TEAM,
         data
     };
 };
 
-export const removeTeam = (data: string) => {
+export const removeTeam = (data: Team) => {
     return {
         type: DELETE_TEAM,
         data
--- src/reducers/teams.ts
+++ src/reducers/teams.ts
@@ -1,20 +1,18 @@
 import teams from '../constants/teams';
-import {addTeam, removeTeam} from '../actions/teams';
+import {addTeam, removeTeam, ADD_TEAM, DELETE_TEAM} from '../actions/teams';
 import {Teams} from '../types';
 
 type TeamsAction = ReturnType<typeof addTeam> | ReturnType<typeof removeTeam>;
 
-const teamsReducer = (prevState: Teams = teams, action: TeamsAction) => {
+const teamList = (prevState: Teams = teams, action: TeamsAction) => {
     switch (action.type) {
-        case 'addteam':
+        case ADD_TEAM:
             return [...prevState, action.data];
-        case 'deleteteam':
-            return prevState.filter(item => {
-                return item.code !== action.data;
-            });
+        case DELETE_TEAM:
+            return prevState.filter(({code}) => code !== action.data.code);
         default:
             return prevState;
     }
 };
 
-export default teamsReducer;
+export default teamList;
