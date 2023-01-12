c87159f7bf60edffcb6617eca0ce69af41076512
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-12 20:03:41 +0900

    feat: 현재 포트변경, 팀 삭제 추가

--- src/actions/pots.ts
+++ src/actions/pots.ts
@@ -1,10 +1,17 @@
-import {Priority, Team} from '../types';
+import {Pot, Team} from '../types';
 
+export const SET_POT = 'pot/setPot' as const;
+export const DELETE_TEAM_FROM_POT = 'pot/deleteTeamFromPot' as const;
 export const ADD_TEAM_TO_POT = 'pot/addTeamToPot' as const;
 
-export const addTeamToPot = (data: {pot: Priority; team: Team}) => {
-    return {
-        type: ADD_TEAM_TO_POT,
-        data
-    };
+export const setPot = (data: Pot) => {
+    return {type: SET_POT, data};
+};
+
+export const deleteTeamFromPot = (data: number) => {
+    return {type: DELETE_TEAM_FROM_POT, data};
+};
+
+export const addTeamToPot = (data: Team) => {
+    return {type: ADD_TEAM_TO_POT, data};
 };
--- src/reducers/pots.ts
+++ src/reducers/pots.ts
@@ -1,26 +1,27 @@
-import {ADD_TEAM_TO_POT, addTeamToPot} from '../actions/pots';
-import {Teams} from '../types';
+import {SET_POT, setPot, DELETE_TEAM_FROM_POT, deleteTeamFromPot, ADD_TEAM_TO_POT, addTeamToPot} from '../actions/pots';
+import {Pot, Teams} from '../types';
+import {pots} from '../constants';
 
-type PotsAction = ReturnType<typeof addTeamToPot>;
+type PotsAction = ReturnType<typeof setPot> | ReturnType<typeof deleteTeamFromPot> | ReturnType<typeof addTeamToPot>;
 type PotsType = {
-    first: Teams;
-    second: Teams;
-    third: Teams;
-    fourth: Teams;
+    currentPot: Pot;
+    teams: Teams;
 };
 const initialPots: PotsType = {
-    first: [],
-    second: [],
-    third: [],
-    fourth: []
+    currentPot: pots[0],
+    teams: []
 };
-const pots = (prevState = initialPots, action: PotsAction) => {
+const potsReducer = (prevState = initialPots, action: PotsAction) => {
     switch (action.type) {
+        case SET_POT:
+            return {...prevState, currentPot: action.data};
+        case DELETE_TEAM_FROM_POT:
+            return {...prevState, teams: prevState.teams.filter((item, index) => index !== action.data)};
         case ADD_TEAM_TO_POT:
-            return {...prevState, [action.data.pot]: [...prevState[action.data.pot], action.data.team]};
+            return {...prevState, teams: [...prevState.teams, action.data].sort(() => Math.random() - 0.5)};
         default:
             return prevState;
     }
 };
 
-export default pots;
+export default potsReducer;
