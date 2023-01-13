748effc403341dbdb9fc4790631bd5fd0f888e61
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-13 23:29:20 +0900

    feat: 그룹 이름 타입 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
+import {Group} from '../types';
 import continents from './continents';
 import pots from './pots';
 import teams from './teams';
 
 const numberOfGroup = 8;
+const groupNames: Array<Group> = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
-export {continents, pots, teams, numberOfGroup};
+export {continents, pots, teams, numberOfGroup, groupNames};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,23 +1,24 @@
-export type Team = {
+export interface Team {
     name: string;
     code: string;
     resource: string;
     continent: string;
-};
-
+}
 export type Teams = Array<Team>;
 
+export type Priority = 'first' | 'second' | 'third' | 'fourth';
+export type Pot = {
+    maxOfGroupTeams: number;
+    code: Priority;
+};
+export type Pots = Array<Pot>;
+
+export type Continent = 'uefa' | 'afc' | 'caf' | 'conmebol' | 'concacaf' | 'ofc';
 export type Continents = Array<{
     name: string;
     code: Continent;
     numberOfEntries: number;
+    maximum: number;
 }>;
 
-export type Priority = 'first' | 'second' | 'third' | 'fourth';
-
-export type Continent = 'uefa' | 'afc' | 'caf' | 'conmebol' | 'concacaf' | 'ofc';
-
-export type Pots = Array<{
-    name: string;
-    code: Priority;
-}>;
+export type Group = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
