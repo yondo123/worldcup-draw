46bcb093e57feb3b7aaf7adc59c6c2a87d1981a9
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-01-16 18:51:46 +0900

    feat: add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <h1>Worldcup Draw</h1>
+            </a>
+        </header>
+    );
+};
+
+export default Header;
